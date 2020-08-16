c02e904e0aa42ddbc204ba37d511daf755d872a6
Rostyslav Ludchenko <skillet367@icloud.com>
2020-08-16 13:27:39 +0300

    changed way to look at the file through the loop

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,76 +1,50 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Parser {
     public static void main(String[] args) throws IOException {
-        List<String> query = new ArrayList<>();
-        List<String> waitingTimeLine = new ArrayList<>();
-        String checkQuery = "";
-        String checkWaitingTimeLine = "";
+        String line = "";
+
+        String[][] splitter = new String[7][6];
+        int currentLine = 0;
 
         try {
-            String text = Files.readString(Paths.get("D:\\analytical_tool\\data.txt"));
-            Scanner myReader = new Scanner(text);
+            File file = new File("D:\\analytical_tool\\data.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
 
-            List<String[]> result = new ArrayList<>();
+            String linesCount = reader.readLine();
 
-            try {
+            line = reader.readLine();
 
-                File file = new File("D:\\analytical_tool\\data.txt");
-                FileReader fr = new FileReader(file);
-                BufferedReader reader = new BufferedReader(fr);
-                String line = reader.readLine();
+            while (line != null) {
+                splitter[currentLine] = line.split(" ");
+                currentLine++;
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-                while (line != null) {
-                    line = reader.readLine();
+        String checkD = "";
+        String checkC = "";
+
+        for (int i = 0; i < splitter.length; i++) {
+            if (splitter[i][0].contains("D")) {
+                for (String s : splitter[i]) {
+                    System.out.print(s + " ");
                 }
+                for (int j = 0; j < i; j++) {
+                    if (splitter[j][0].contains("C")) {
+                        for (String s : splitter[j]) {
 
-                while (myReader.hasNextLine()) {
-                    result.add(myReader.nextLine().split(" "));
+                        }
+                        System.out.println();
+                    }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-
-//            for (String s : result) {
-//                if (s.startsWith("D")) {
-//                    query.add(s);
-//                }
-//
-//                if (s.startsWith("C")) {
-//                    waitingTimeLine.add(s);
-//                }
-//            }
-
-            List<String> updatedQuery = new ArrayList<>();
-            List<String> updatedWaitingTimeLine = new ArrayList<>();
-
-            for (String s : query) {
-                checkQuery = s.substring(s.indexOf(' ') + 1);
-                updatedQuery.add(checkQuery);
-            }
-
-            for (String w : waitingTimeLine) {
-                checkWaitingTimeLine = w.substring(w.indexOf(' ') + 1);
-                updatedWaitingTimeLine.add(checkWaitingTimeLine);
-            }
-
-            for (String s : updatedQuery) {
-                System.out.println(s);
-            }
-
-            myReader.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.!");
         }
     }
 }
