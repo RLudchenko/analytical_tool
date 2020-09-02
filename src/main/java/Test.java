d8dcde6f5404b6792f913bfee3bfe81831de5a42
Rostyslav Ludchenko <skillet367@icloud.com>
2020-09-02 13:39:41 +0300

    added solution with different variations of inputs

--- src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String s = "1122-1121";
-        String m = s.replaceAll("-", "");
-        System.out.println(m);
-    }
-}
