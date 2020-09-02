c533f7aaa4ce53f0e3252741b32bd8dc2a6ee28f
Rostyslav Ludchenko <skillet367@icloud.com>
2020-09-02 13:41:27 +0300

    removed comments, introduced instruction in Readme file

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,24 +3,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class Parser {
     public static void main(String[] args) throws IOException, ParseException {
         String line = "";
+        String path = "D:\\analytical_tool\\data.txt";
 
         String[][] splitter = new String[7][6];
         int currentLine = 0;
 
         try {
-            File file = new File("D:\\analytical_tool\\data.txt");
+            File file = new File(path);
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
 
-            String linesCount = reader.readLine();
+            reader.readLine();
 
             line = reader.readLine();
 
@@ -38,34 +37,38 @@ public class Parser {
             int count = 0;
             int average;
 
-            String dDateStart = "";
-            String dDateEnd = "";
-            String cDate = "";
+            String dateDStart = "";
+            String dateDEnd = "";
+            String dateC = "";
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-            LocalDate dDateS = LocalDate.now();
-            LocalDate dDateE = LocalDate.now();
-            LocalDate dateC = LocalDate.now();
+            LocalDate ddateS = LocalDate.now();
+            LocalDate ddateE = LocalDate.now();
+            LocalDate cdateInit;
 
             if (splitter[i][0].contains("D")) {
                 for (int j = 0; j < i; j++) {
                     if (splitter[i][4].contains("-")) {
                         String[] split = splitter[i][4].split("-");
                         for (int k = 0; k < split.length; k++) {
-                            dDateStart = split[0];
-                            dDateS = LocalDate.parse(dDateStart, formatter);
-                            dDateEnd = split[1];
-                            dDateE = LocalDate.parse(dDateEnd, formatter);
+                            dateDStart = split[0];
+                            ddateS = LocalDate.parse(dateDStart, formatter);
+                            dateDEnd = split[1];
+                            ddateE = LocalDate.parse(dateDEnd, formatter);
                         }
                     }
 
-                    //todo if not within the range? continue
                     if (splitter[j][0].contains("C")) {
-                        cDate = splitter[j][4];
-                        if (splitter[i][1].equals(splitter[j][1].substring(0, 1))
-                                && splitter[i][2].equals(splitter[j][2].substring(0, 1))
-                                || splitter[i][2].equals("*")) {
+                        dateC = splitter[j][4];
+                        cdateInit = LocalDate.parse(dateC, formatter);
+                        if ((splitter[i][1].equals(splitter[j][1].substring(0, 1))
+                                || splitter[i][1].equals("*")
+                                || splitter[j][1].equals("*"))
+                                && (splitter[i][2].equals(splitter[j][2].substring(0, 1))
+                                || splitter[i][2].equals("*")
+                                || splitter[j][2].equals("*"))
+                                && ddateS.compareTo(cdateInit) * cdateInit.compareTo(ddateE) > 0) {
                             count++;
                             sum += Integer.parseInt(splitter[j][splitter[j].length - 1]);
                             continue;
@@ -76,16 +79,14 @@ public class Parser {
                             count++;
                             sum += Integer.parseInt(splitter[j][splitter[j].length - 1]);
                         }
-                        // compare splitterp[i] = D String
-                        // with splitter[j] - c String
                     }
                 }
-
-                System.out.println("START: " + dDateS + " END: " + dDateE);
 
                 if (count > 1) {
                     average = sum / count;
                     System.out.println(average);
+                } else if (sum == 0) {
+                    System.out.println("-");
                 } else {
                     System.out.println(sum);
                 }
